eecb7a3bf8ae7d706530f06765b308617dc281fb
Emmanuel6093 <elorenzo6093@gmail.com>
2022-08-23 21:05:20 -0400

    homepage complete

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import heroImage from '../public/assets/heroImage.png'
 import {MdExpandMore} from 'react-icons/md'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const HomePage = () => {
   return (
@@ -18,6 +19,15 @@ const HomePage = () => {
           <p className='text-gray-600 text-xl max-w-sm mx-auto'>
             I have 1 year of experience of building and designing web applications. Currently, I love to work on web applications using technologies like React, Tailwind, and Next JS. 
           </p>
+
+          <Link href='/#me'>
+            <div className='group flex items-center justify-center my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider'>
+              know more 
+              <span className='-rotate-90 duration-100 ease-in group-hover:rotate-0'>
+                <MdExpandMore size={25} />
+              </span>
+            </div>
+          </Link>
 
       </div>
     </div>
