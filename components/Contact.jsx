d3c74ca028c3e2354f8092da0ab2ec4076da66fe
Emmanuel6093 <elorenzo6093@gmail.com>
2022-08-25 21:00:49 -0400

    imported social icons to contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import React from "react";
 
-import contactUs from '../public/assets/contact-us.jpg'
+import contactUs from "../public/assets/contact-us.jpg";
+import {FaFacebook, FaGithub, FaTwitter, FaLinkedin} from 'react-icons/fa';
 
 const Contact = () => {
   return (
@@ -11,22 +12,39 @@ const Contact = () => {
           contact
         </h2>
 
-        <div className="flex flex-col md:flex-row gap-8 shadow-xl shadow-blue-300 bg-white">
+        <div className="flex flex-col md:flex-row gap-8 shadow-xl shadow-blue-300 bg-white mt-4">
+          <div className="w-full md:w-1/2 h-full rounded-xl p-4">
+            <Image
+              src={contactUs}
+              alt="man with laptop"
+              className="rounded-xl"
+            />
+            <p className="pt-2 pb-8">
+                I am open to talk regarding full time opportunities. Feel free contacting me using your preferred method.
+            </p>
 
-            <div className="w-full md:w-1/2 h-full rounded-xl p-4">
-                <Image 
-                    src={contactUs} 
-                    alt='man with laptop' 
-                    className="rounded-xl" 
-                />
-            </div>
+            
+            <div className="grid grid-cols-2 mx-auto w-4/5 gap-10">
+                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
+                  <FaLinkedin size={25} />
+                </div>
+                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
+                  <FaTwitter size={25} />
+                </div>
+                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
+                  <FaFacebook size={25} />
+                </div>
+                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
+                  <FaGithub size={25} />
+                </div>
+              </div>
 
 
-            {/* form */}
-            <div className="w-full md:w-1/2 h-full rounded-xl p-4"></div>
+          </div>
 
+          {/* form */}
+          <div className="w-full md:w-1/2 h-full rounded-xl p-4"></div>
         </div>
-
       </div>
     </div>
   );
