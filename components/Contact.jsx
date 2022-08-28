40e169f5e04e8bd7d63c52eec098d233a399ff5e
Emmanuel6093 <elorenzo6093@gmail.com>
2022-08-28 18:45:36 -0400

    form inputs added

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 import contactUs from "../public/assets/contact-us.jpg";
-import {FaFacebook, FaGithub, FaTwitter, FaLinkedin} from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -20,30 +20,77 @@ const Contact = () => {
               className="rounded-xl"
             />
             <p className="pt-2 pb-8">
-                I am open to talk regarding full time opportunities. Feel free contacting me using your preferred method.
+              I am open to talk regarding full time opportunities. Feel free
+              contacting me using your preferred method.
             </p>
 
-            
             <div className="grid grid-cols-2 mx-auto w-4/5 gap-10">
-                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                  <FaLinkedin size={25} />
-                </div>
-                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                  <FaTwitter size={25} />
-                </div>
-                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                  <FaFacebook size={25} />
-                </div>
-                <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                  <FaGithub size={25} />
-                </div>
+              <div className="flex items-center justify-center rounded-full shadow-md shadow-blue-500 hover:scale-105 duration-200 p-3 cursor-pointer">
+                <FaLinkedin size={25} />
               </div>
-
-
+              <div className="flex items-center justify-center rounded-full shadow-md shadow-blue-500 hover:scale-105 duration-200 p-3 cursor-pointer">
+                <FaTwitter size={25} />
+              </div>
+              <div className="flex items-center justify-center rounded-full shadow-md shadow-blue-500 hover:scale-105 duration-200 p-3 cursor-pointer">
+                <FaFacebook size={25} />
+              </div>
+              <div className="flex items-center justify-center rounded-full shadow-md shadow-blue-500 hover:scale-105 duration-200 p-3 cursor-pointer">
+                <FaGithub size={25} />
+              </div>
+            </div>
           </div>
 
           {/* form */}
-          <div className="w-full md:w-1/2 h-full rounded-xl p-4"></div>
+          <div className="w-full md:w-1/2 h-full rounded-xl p-4">
+            <div className="p-4 text-left">
+              <form action="">
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                <div className="flex flex-col">
+                    <label className="capitalize text-sm py-2 text-slate-600 font-light">
+                      name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      className="border-2 rounded-lg p-3 flex focus:outline-none border-blue-200"
+                    />
+                  </div>    <div className="flex flex-col">
+                    <label className="capitalize text-sm py-2 text-slate-600 font-light">
+                      phone
+                    </label>
+                    <input
+                      type="text"
+                      name="phone"
+                      className="border-2 rounded-lg p-3 flex focus:outline-none border-blue-200"
+                    />
+                  </div>    <div className="flex flex-col col-span-2">
+                    <label className="capitalize text-sm py-2 text-slate-600 font-light">
+                      email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      className="border-2 rounded-lg p-3 flex focus:outline-none border-blue-200"
+                    />
+                  </div>    <div className="flex flex-col col-span-2">
+                    <label className="capitalize text-sm py-2 text-slate-600 font-light">
+                      message
+                    </label>
+                   <textarea 
+                   name="message"  
+                   rows="10" 
+                   className="border-2 rounded-lg p-3 flex focus:outline-none border-blue-200"
+                   ></textarea>
+                  </div>
+                </div>
+
+                <div className="flex items-center justify-center">
+                    <button className="my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer hover:scale-105 duration-200">send message</button>
+                </div>
+
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
